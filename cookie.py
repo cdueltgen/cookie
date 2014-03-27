d65289f62d0a976bf11be4decce471817f4e8caf
cdueltgen <cdueltgen@gmail.com>
2014-03-27 14:10:11 -0700

    working with corrected councils dict

--- cookie.py
+++ cookie.py
@@ -3,6 +3,7 @@ import requests
 from pyquery import PyQuery
 from bs4 import BeautifulSoup as BS
 
+from councils_with_gf_cookies import councils
 # This is how you use requests to get data
 # WRONG you's think this would work, but it doesn't. See below for curl command
 # r = requests.post("https://www.girlscouts.org/councilfinder/results.asp", data="Zip=94703")
@@ -42,13 +43,29 @@ def soup_me(html):
     # This finds all the ones that are bold, council is last one?
     soup = BS(html)
     b = soup.find_all('b')
-    print b[-1].text
+    return b[-1].text
+
+# THIS FUNCTION NO LONGER NEEDED
+def get_councils():
+    councils_dict = {}
+    f = open("data/councils_with_gf_cookies.txt")
+    for line in f:
+        councils_dict[line.strip()] = 1
+    f.close()
+    return councils_dict
 
 def main():
-    test_zips = ['01060', '02199', '10557', '17765', '26717', '28072', '36908', '45740', '62524', '74438', '94703']
+    test_zips = ['56387', '01060', '02199', '10557', '17765', '26717', '28072', '36908', '45740', '62524', '74438', '94703']
+    # councils = get_councils()
     for zc in test_zips:
         r = requestHTML(zc)
         html = make_text(r)
-        soup_me(html)
+        q_council = soup_me(html)
+        if councils.get(q_council):
+            print "%s has gluten free cookies!" % q_council
+        else:
+            print "%s does not have gluten free cookies" % q_council
 
-main()
+if __name__ == "__main__":
+    # main()
+    pass
